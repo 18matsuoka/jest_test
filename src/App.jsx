ce071bda149f10a82d0a31894b6736f7fc17352f
FUTURE\matsuoka3548 <y.matsuoka.dz@future.co.jp>
2022-09-10 02:51:13 +0900

    20220912課題

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,85 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import styled from 'styled-components'
+import { Button, TextField } from '@mui/material'
 
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  const Menu = styled.div`
+const Menu = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-  const Logo = styled.div`
+const Logo = styled.div`
   width: 200px;
 `;
-  const Header = styled.div`
+const Header = styled.div`
   width: 1000px;
   display: flex;
   // background: pink;
   justify-content: space-between;
 `;
 
-  const Button = styled.div`
+const SButton = styled.div`
   background: #D9D9D9;
   border-radius: 20px;
   margin: 0 10px;
   width: 100px;
 `
 
-  const Body = styled.div`
+const Body = styled.div`
   width: 1000px;
-  height: 500px;
+  min-height: 500px;
 `
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`
+
+const BodyContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 30px;
+  width:100%;
+`
+
+const BodyItem = styled.div`
+  width: 30%;
+  height: 300px;
+  border: 1px solid #000;
+  margin: 5px;
+  padding:5px;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+`
+
+
+function App() {
+  const [datas, setData] = useState([]);
+
+  const [word, setWord] = useState();
+
+  const arr = ["1", "2", "3","4","5"];
+
+
+  const search = async () => {
+
+    if (!word) {
+      alert("1文字以上入力してください。");
+      return true;
+    }
+
+    const response = await fetch(
+      `https://livlog.xyz/webapi/springWater?q=${word}`
+    ).then((res) => res.json())
+      .then((cdata) => {
+        setData(cdata);
+      });
+  }
 
   return (
     <div className="App">
@@ -46,12 +93,27 @@ function App() {
         {/* Menu */}
         <Menu>
           {/* MenuItem */}
-          <Button>HELP</Button>
-          <Button>LOGIN</Button>
+          <SButton>HELP</SButton>
+          <SButton>LOGIN</SButton>
         </Menu>
       </Header>
       <hr />
-      <Body>コンテンツ</Body>
+      <Body>
+        <h2>湧き水検索</h2>
+        <MainContainer>
+          <TextField id="standard-basic" label="湧き水がある場所を検索する" variant="standard" onChange={(e) => setWord(e.target.value)} />
+          <Button onClick={search} variant="outlined" sx={{ marginLeft: "5px" }}>検索する</Button>
+        </MainContainer>
+        <BodyContainer>
+          {!(datas.length ===0) && datas.result.map((item,index)=>(
+              <BodyItem key={index}>
+                <h3>{item.name}({item.furigana})</h3>
+                <p>{item.overview}</p>
+                <p style={{marginTop:'auto'}}><a href={"https://www.google.com/search?q="+item.name} target="_blank" rel="noopener noreferrer">Googleで検索する</a></p>
+              </BodyItem>
+          ))}
+        </BodyContainer>
+      </Body>
     </div>
   )
 }
